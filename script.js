3738ecd43ec753ff7a88ffd831da334c0fd5e4bb
jang-3 <148173571+jang-3@users.noreply.github.com>
2025-06-02 21:16:24 +0800

    changed visuals

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let barcolour = document.getElementById("header-bar");
+let barlink = document.querySelectorAll(".link");
 
+function barChange() {
+  if (window.scrollY > 2900) {
+    barcolour.style.backgroundColor = "white";
+    barlink.forEach((link) => (link.style.color = "black"));
+  } else {
+    barcolour.style.backgroundColor = "black";
+    barlink.forEach((link) => (link.style.color = "rgb(255, 125, 3)"));
+  }
+}
+
+window.addEventListener("scroll", barChange);
